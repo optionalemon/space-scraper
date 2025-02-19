0de5748df3585560564ba08f6d9c8e7ca060a648
Ann <cchouaa@connect.ust.hk>
2025-02-19 21:16:49 +0800

    updated grab and shoot function

--- Assets/Script/GunController.cs
+++ Assets/Script/GunController.cs
@@ -10,6 +10,8 @@ public class GunController : MonoBehaviour
     public float secondsBetweenShoot = 0.2f;
     float trackShoot;
 
+    private int bullets = 0;
+
     void Start()
     {
    	shoot.action.Enable(); 
@@ -18,13 +20,18 @@ public class GunController : MonoBehaviour
     void Update() 
     {
         Debug.Log(shoot.action.ReadValue<float>());
-        if (shoot.action.ReadValue<float>() == 1 & trackShoot <= 0)
+        if (shoot.action.ReadValue<float>() == 1 & trackShoot <= 0 && bullets > 0)
         {
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             bullet.transform.rotation = firePoint.rotation * Quaternion.Euler(90, 0, 0);
             trackShoot = secondsBetweenShoot;
+            bullets--;
         }
         trackShoot -= Time.deltaTime;
+    }
+
+    public void loadBullets() {
+        bullets += 10;
     }
 }
 
